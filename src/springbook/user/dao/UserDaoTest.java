07d658fb644afdcd3b1120cb76da0df3c4c4b6c5
weedew <weeddew@gmail.com>
2012-09-10 16:10:21 +0900

    changed

--- src/springbook/user/dao/UserDaoTest.java
+++ src/springbook/user/dao/UserDaoTest.java
@@ -2,14 +2,17 @@ package springbook.user.dao;
 
 import java.sql.SQLException;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 import springbook.user.domain.User;
 
 public class UserDaoTest {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
-		DaoFactory factory = new DaoFactory();
-		UserDao dao = factory.userDao();
+		ApplicationContext context = new GenericXmlApplicationContext("/applicationContext_test.xml");
+		UserDao dao = context.getBean("userDao", UserDao.class);
 
 		User user = new User();
 		user.setId("brownie");
